97e90e74232684ab179c4785b276ee0f754f96fe
BL00D-C0MMANDER <97792908+BL00D-C0MMANDER@users.noreply.github.com>
2024-03-16 17:27:01 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,11 @@
+import random 
+import sys
+import pygame
+import pygame.local import *
+
+# Global variables
+FPS = 32
+SCREENWIDTH = 289
+SCREENHEIGHT = 511
+SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+GROUNDY = SCREENHEIGHT * 0.8
